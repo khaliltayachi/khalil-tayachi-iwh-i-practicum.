426f96906436d9f11c7c672a851e16e88bea71a8
khaliltayachi <tayachy.khalil@gmail.com>
2024-07-22 12:32:34 +0100

    Update code to use environment variables and ensure no sensitive information is included

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
+require('dotenv').config();
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PRIVATE_APP_ACCESS = 'pat-eu1-ab061ffa-58c8-4aa2-ae5c-d417254f6370'; // Replace with your private app access token
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
